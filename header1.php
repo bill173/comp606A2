fdea1ea5320e966741f8711ad146d7384ec0e68a
bill173 <nzbillliu@gmail.com>
2019-11-01 19:35:47 +1300

    header

---
+++ header1.php
@@ -0,0 +1,28 @@
+<?php  
+require "lib/autoloader.php";
+?>
+
+
+
+
+
+<!--导航-->
+<div class="header">
+   
+
+    <nav>
+
+    
+    
+        <ul class="word clearfix">
+        <li><a href="myEstimatedJob.php">MY ESTIMATED JOB</a></li>
+           
+            <li><a href="myPostedJob.php?cid=2">MY POSTED JOB</a></li>
+            <li><a href="post.php?cid=1">POST A JOB</a></li>
+            <li><a href="showJobList.php?cid=4">JOB LIST</a></li>
+          
+        </ul>
+    </nav>
+    
+</div>
+
